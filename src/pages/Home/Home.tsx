9eaa4d2a5f84de0464d0be98735a7a31e0a2a6bc
dan95 el guay <ddm19dev@gmail.com>
2025-07-02 23:21:59 +0200

    fix: styles reunited

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,6 @@ const Home: React.FC = () => {
   return (
     <div className="Home">
       <h1>Welcome to the Food Contest Voting App</h1>
-      <p>Vote for your favorite dishes and help us find the best food!</p>
     </div>
   );
 }
