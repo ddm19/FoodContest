a4b38f82499ec802a1146757db4cb6f19803b372
dan95 el guay <ddm19dev@gmail.com>
2025-07-24 23:52:44 +0200

    feat: all sections

--- src/pages/results/testresults.tsx
+++ src/pages/results/testresults.tsx
@@ -2,31 +2,43 @@ import { useState, useEffect } from "react";
 import { getVotesByColorAndCategory } from "supabase/pollActions";
 import ResultsPanel, { Category } from "./components/resultsPanel/resultsPanel";
 import { colors } from "pages/poll/constants";
-
+import CustomTabs, { TabItem } from "components/customTabs/customTabs";
+import Loading from "components/Loading/Loading";
 
 const TestResults = () => {
-    const [votes, setVotes] = useState([]);
+    const [votesByCategory, setVotesByCategory] = useState<Record<string, any[]>>({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        getVotesByColorAndCategory(colors.map(color => color.name), "flavour")
-            .then((data) => {
-                if (data) {
-                    setVotes(data);
-                    console.log("Votes fetched:", data);
-                }
-            })
-            .finally(() => {
+        const fetchAllVotes = async () => {
+            try {
+                const votePromises = Object.entries(Category).map(async ([key, categoryName]) => {
+                    const data = await getVotesByColorAndCategory(colors.map(c => c.name), key);
+                    return [categoryName, data || []];
+                });
+                const results = await Promise.all(votePromises);
+                const finalVotesObject = Object.fromEntries(results);
+                setVotesByCategory(finalVotesObject);
+            } catch (error) {
+                console.error("Error fetching votes:", error);
+            } finally {
                 setLoading(false);
-            });
+            }
+        };
+        fetchAllVotes();
     }, []);
 
+    if (loading || Object.keys(votesByCategory).length === 0) {
+        return <Loading text="Cargando resultados..." />;
+    }
+
+    const tabs: TabItem[] = Object.entries(votesByCategory).map(([categoryName, votesData]) => ({
+        title: categoryName,
+        content: <ResultsPanel key={categoryName} category={categoryName as Category} votes={votesData} />
+    }));
 
     return (
-        <ResultsPanel
-            category={Category.flavour}
-            votes={votes}
-        />
+        <CustomTabs tabs={tabs} />
     );
 };
 
